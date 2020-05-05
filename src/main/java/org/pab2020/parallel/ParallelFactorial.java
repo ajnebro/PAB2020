8a6ef94b114590bdd60a73be8025053490db6c0d
ajnebro <ajnebro@users.noreply.github.com>
2020-05-05 12:18:52 +0200

    Minor changes

--- src/main/java/org/pab2020/parallel/ParallelFactorial.java
+++ src/main/java/org/pab2020/parallel/ParallelFactorial.java
@@ -8,15 +8,18 @@ import java.util.stream.Collectors;
 
 public class ParallelFactorial {
   public static void main(String[] args) {
-    List<Integer> numbers = List.of(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13) ;
+    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16) ;
     List<Long> factorialValues ;
     HeavyFactorial factorial = new HeavyFactorial() ;
 
     long initTime = System.currentTimeMillis() ;
 
+    int numberOfThreads = 1;
+    System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "" + numberOfThreads);
+
     factorialValues = numbers
-            .stream()
-            .map(number -> factorial.compute(number))
+            .parallelStream()
+            .map(factorial::compute)
             .collect(Collectors.toList());
 
     long totalTime = System.currentTimeMillis() - initTime ;
